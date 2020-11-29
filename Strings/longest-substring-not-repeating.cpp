9aad868f8f5072b4d6867090ad194a28893d4b64
Luc Meric <mericLuc@hotmail.fr>
2020-11-29 17:51:16 +0100

    [Strings]: Added a simple question on substrings

---
+++ Strings/longest-substring-not-repeating.cpp
@@ -0,0 +1,18 @@
+/*!
+ * @description: Find the longest substring without any
+ *               repeating character.
+ */
+
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        vector<int> cache(256, -1);
+        int l(-1), res(0);
+        for (int i = 0; i < s.size(); i++) {
+            l   = max( l   , cache[s[i]] );
+            res = max( res , i - l       );
+            cache[s[i]] = i;
+        }
+        return res;
+    }
+};
