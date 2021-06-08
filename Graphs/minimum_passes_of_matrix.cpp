fe702586a8ab1ed0f54071792ba95cf5b40845fa
lhm <mericluc@hotmail.fr>
2021-06-08 15:47:07 +0200

    [FIX]: Typos

--- Graphs/minimum_passes_of_matrix.cpp
+++ Graphs/minimum_passes_of_matrix.cpp
@@ -11,44 +11,44 @@ template<typename T>
 using Mat = std::vector< std::vector<T>>;
 
 bool isPositive(const Mat<int>& m, 
-								const int&      x, 
-								const int&      y) {
+                const int&      x, 
+                const int&      y) {
 	return (x >= 0) && (x < std::size( m )    ) && 
-		     (y >= 0) && (y < std::size( m[0] ) ) &&
-		     (m[x][y] > 0);
+	       (y >= 0) && (y < std::size( m[0] ) ) &&
+	       (m[x][y] > 0);
 }
 
-bool convert(const Mat<int>&  m, 
-						 Mat<int>& 			  cpy,
-						 const int&       x, 
-						 const int&       y) {
-	if ( (m[x][y] < 0) &&
-		 ( isPositive(m, x - 1, y    ) || isPositive(m, x + 1, y    ) || 
-			 isPositive(m, x    , y - 1) || isPositive(m, x    , y + 1) ) ) {
-		cpy[x][y] *= -1;
-		return true;
+bool convert(const Mat<int>&  m,
+             Mat<int>& 	      cpy,
+             const int&       x, 
+             const int&       y) {
+    if ( (m[x][y] < 0) &&
+        ( isPositive(m, x - 1, y    ) || isPositive(m, x + 1, y    ) || 
+          isPositive(m, x    , y - 1) || isPositive(m, x    , y + 1) ) ) {
+        cpy[x][y] *= -1;
+	    return true;
 	}
 	return false;
 }
 
 int minimumPassesOfMatrix(Mat<int> m) {
-  int  l_res {0};
-	bool l_keepGoing{true};
+    int  l_res      { 0 };
+    bool l_keepGoing{ true };
 	
-	while( l_keepGoing ) {
-		auto m_cpy = m;
-		l_keepGoing = false;
-		for   ( auto i = 0; i < std::size( m    ); ++i )
-			for ( auto j = 0; j < std::size( m[0] ); ++j ) {
-				l_keepGoing |= convert( m, m_cpy, i, j );
-				if ( l_keepGoing ) continue;
-			}
-		std::swap(m, m_cpy);
-		if ( l_keepGoing ) { ++l_res; }
-	}
-	
-	for   ( auto i = 0; i < std::size( m    ); ++i )
-		for ( auto j = 0; j < std::size( m[0] ); ++j )
-			if ( m[i][j] < 0 ) return -1;
-  return l_res;
+    while( l_keepGoing ) {
+        auto m_cpy = m;
+        l_keepGoing = false;
+        for     ( auto i = 0; i < std::size( m    ); ++i )
+            for ( auto j = 0; j < std::size( m[0] ); ++j ) {
+                l_keepGoing |= convert( m, m_cpy, i, j );
+                if ( l_keepGoing ) continue;
+            }
+        std::swap(m, m_cpy);
+        if ( l_keepGoing ) { ++l_res; }
+    }
+    
+    for      ( auto i = 0; i < std::size( m    ); ++i )
+        for ( auto j = 0; j < std::size( m[0] ); ++j )
+            if ( m[i][j] < 0 ) return -1;
+    return l_res;
 }
